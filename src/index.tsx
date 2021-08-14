fa8cde1bf6f8289606a7abc2d80db4f2d0008e4a
caohuijun2018 <1160364324@qq.com>
2021-08-14 20:12:10 +0800

    feat:Completing  the function of calculating weights of CSS selector

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const CSS_SELECTOR_REGEX_MAP: Record<string, RegExp> = {
+  id: /(#[^#\s+>~.[:)]+)/g,
+  class: /(\.[^\s+>~.[:)]+)/g,
+  attribute: /(\[[^\]]+\])/g,
+  pseudoClass: /(:(?!not|global|local)[^\s+>~.[:]+)/g,
+  tag: /([^\s+>~.[:]+)/g,
+  pseudoElement: /(::[^\s+>~.[:]+|:first-line|:first-letter|:before|:after)/gi,
+};
+const CSS_SELECTOR_PRIORITY_MAP: Record<string, number> = {
+  attribute: 100,
+  id: 1000,
+  class: 100,
+  tag: 10,
+  pseudoElement: 10,
+  pseudoClass: 100,
+};
+export function calculatePriority(input: string) {
+  let text = input;
+  return Object.keys(CSS_SELECTOR_REGEX_MAP).reduce((pre, key) => {
+    const regex = CSS_SELECTOR_REGEX_MAP[key];
+    let count = 0;
+    while (true) {
+      const rexArray = regex.exec(text);
+      if (rexArray) {
+        count++;
+        text = text.replace(rexArray[0], "");
+      } else {
+        break;
+      }
+    }
+    return pre + count * CSS_SELECTOR_PRIORITY_MAP[key];
+  }, 0);
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export function compare(a: string, b: string) {
+  const priority_a = calculatePriority(a);
+  const priority_b = calculatePriority(b);
+  return priority_a - priority_b;
+}
